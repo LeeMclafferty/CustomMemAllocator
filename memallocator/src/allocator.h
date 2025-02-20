226541e3e97b4921445124a68853a838736c6bd4
Lee McLafferty <lee.mclafferty@gmail.com>
2025-02-19 18:40:42 -0800

    Updated request and free functions to check and reuse blocks instead of always deleting and allocating more memory.

--- memallocator/src/allocator.h
+++ memallocator/src/allocator.h
@@ -14,43 +14,72 @@
 
 struct MemBlock {
 	MemBlock() {}
-	MemBlock(size_t s, void* ad, MemBlock* p, MemBlock* n)
-		:size(s), address(ad), previous(p), next(n) {}
+	MemBlock(size_t s, void* ad, MemBlock* p, MemBlock* n, bool f)
+		:size(s), address(ad), previous(p), next(n), free() {}
 
 	void* address;
 	MemBlock* previous;
 	MemBlock* next;
 	size_t size;
+	bool free = false;
 };
 
 MemBlock* head = nullptr;
 
 void* request_mem(size_t size) {
+	MemBlock* buffer = head;
+	while (buffer) {
+		if (!buffer->free) {
+			continue;
+		}
+
+		if (buffer->size == size) {
+			return buffer->address;
+		}
+	} 
+
 	void* address = VirtualAlloc(nullptr, size, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
-	MemBlock* block = new MemBlock(size, address, nullptr, head);
+	MemBlock* block = new MemBlock(size, address, nullptr, head, false);
 	if (head) {
 		head->previous = block;
 	}
 	head = block;
+
+	return block->address;
 }
 
-void free_mem(void* ptr) {
+/*
+ Parameter
+	ptr - memory address that you want to free
+	free_block - Default value is false, which will delete the block from memory.
+	if free_block is set to true, the block will remain in memory as a "free block" for 
+	reuse. 
+*/
+void free_mem(void* ptr, bool free_block = false) {
 	MemBlock* buffer = head;
 	while (buffer) {
 		if (buffer->address == ptr) {
-			if (buffer->previous) {
-				buffer->previous->next = buffer->next;
-			}
-			if (buffer->next && buffer->next->previous) {
-				buffer->next->previous = buffer->previous;
-			}
+			if (!free_block) {
+				if (buffer->previous) {
+					buffer->previous->next = buffer->next;
+				}
+				if (buffer->next && buffer->next->previous) {
+					buffer->next->previous = buffer->previous;
+				}
 
-			if (buffer == head) {
-				head = buffer->next;
+				if (buffer == head) {
+					head = buffer->next;
+				}
+	
+				VirtualFree(ptr, 0, MEM_RELEASE);
+				delete buffer;
+				break;
 			}
-			VirtualFree(ptr, 0, MEM_RELEASE);
-			delete buffer;
-			break;
+			else
+			{
+				buffer->free = true;
+			}
+	
 		}
 		buffer = buffer->next;
 	}
